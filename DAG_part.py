06f5e276d9d74fcaafe0fa4e34c83f637f29f119
Vitaly Zaitsev <vvzaitsev79@gmail.com>
2021-06-03 12:37:54 +0300

    ДЗ по уроку 6

---
+++ DAG_part.py
@@ -0,0 +1,32 @@
+from airflow.models import DAG
+from operators.postgres import DataTransferPostgres
+from datetime import datetime
+
+DEFAULT_ARGS = {
+    "owner": "airflow",
+    "start_date": datetime(2021, 1, 25),
+    "retries": 1,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "depends_on_past": False, # в случае регулярной эксплуатации следует поставить True. Сейчас указал False для целей отладки
+}
+
+with DAG(
+    dag_id="DataFlow-part",
+    default_args=DEFAULT_ARGS,
+    schedule_interval="@daily",
+    max_active_runs=1,
+    tags=['data-flow'],
+) as dag1:
+    # сам data flow, для таблицы part
+    data_transfer = DataTransferPostgres(
+        config={'table': 'public.part'},
+        query='select * from part',
+        task_id='part',
+        source_pg_conn_str="host='host.docker.internal' port=5433 dbname='my_database_1' user='root' password='postgres'",
+        pg_conn_str="host='host.docker.internal' port=54320 dbname='my_database_2' user='root' password='postgres'",
+    )
+    
+    # Порядок выполнения тасок
+    data_transfer
+
---
+++ DAG_supplier.py
@@ -0,0 +1,32 @@
+from airflow.models import DAG
+from operators.postgres import DataTransferPostgres
+from datetime import datetime
+
+DEFAULT_ARGS = {
+    "owner": "airflow",
+    "start_date": datetime(2021, 1, 25),
+    "retries": 1,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "depends_on_past": False, # в случае регулярной эксплуатации следует поставить True. Сейчас указал False для целей отладки
+}
+
+with DAG(
+    dag_id="DataFlow-supplier",
+    default_args=DEFAULT_ARGS,
+    schedule_interval="@daily",
+    max_active_runs=1,
+    tags=['data-flow'],
+) as dag1:
+    # сам data flow, для таблицы supplier
+    data_transfer = DataTransferPostgres(
+        config={'table': 'public.supplier'},
+        query='select * from supplier',
+        task_id='supplier',
+        source_pg_conn_str="host='host.docker.internal' port=5433 dbname='my_database_1' user='root' password='postgres'",
+        pg_conn_str="host='host.docker.internal' port=54320 dbname='my_database_2' user='root' password='postgres'",
+    )
+    
+    # Порядок выполнения тасок
+    data_transfer
+
