03b62dd26a47943901e7ad3fd5f6226e9b075f0a
Vitaly Zaitsev <vvzaitsev79@gmail.com>
2021-06-14 18:00:26 +0300

    Final homework

---
+++ Final/dags/operators/data_transfer.py
@@ -0,0 +1,81 @@
+import datetime
+import logging
+import os
+import time
+from contextlib import contextmanager
+
+import psycopg2
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+from operators.utils import DataFlowBaseOperator
+
+
+class DataTransfer(DataFlowBaseOperator):  # modify
+    @apply_defaults
+    def __init__(self, config, target_pg_conn_str,  date_check=True, *args, **kwargs):
+        super(DataTransfer, self).__init__(
+            *args,
+            **kwargs
+        )
+        self.config = config
+        self.target_pg_conn_str = target_pg_conn_str
+        self.date_check = date_check
+
+    def provide_data(self, csv_file, context):
+        pass
+
+    def execute(self, context):
+        copy_statement = """
+        COPY {target_schema}.{target_table} ({columns}, launch_id) FROM STDIN with
+        DELIMITER '\t'
+        CSV
+        ESCAPE '\\'
+        NULL '';
+        """
+        schema_name = "{table}".format(**self.config).split(".")
+        self.config.update(
+            target_schema=schema_name[0],
+            target_table=schema_name[1],
+            job_id=context["task_instance"].job_id,  # modify
+            dt=context["task_instance"].execution_date,  # modify
+        )
+        # modify
+        if self.date_check and context["execution_date"] in self.get_load_dates(
+            self.config
+        ):
+            logging.info("Data already load")
+            return
+        with psycopg2.connect(self.target_pg_conn_str) as conn, conn.cursor() as cursor:
+            start = time.time()  # modify
+            # modify
+            cursor.execute(
+                """
+            set search_path to logstats; 
+            select column_name
+              from information_schema.columns
+             where table_schema = '{target_schema}'
+               and table_name = '{target_table}'
+               and column_name not in ('launch_id', 'effective_dttm');
+            """.format(
+                    **self.config
+                )
+            )
+            result = cursor.fetchall()
+            columns = ", ".join('"{}"'.format(row) for row, in result)
+            self.config.update(columns=columns)
+
+            with open("./dags/transfer.csv", "w", encoding="utf-8") as csv_file:
+                self.provide_data(csv_file, context)
+
+            self.log.info("writing succed")
+
+            with open('./dags/transfer.csv', 'r', encoding="utf-8") as f:
+                cursor.copy_expert(copy_statement.format(**self.config), f)
+
+            self.config.update(  # modify
+                launch_id=-1,
+                duration=datetime.timedelta(seconds=time.time() - start),
+                row_count=cursor.rowcount
+            )
+            self.write_etl_log(self.config)  # modify
